4cc164d2c998de7e9be9220dfa7f552e6bf13c81
Mayank Govilla <mgovilla@gmail.com>
2021-06-09 16:10:39 -0400

    Adapt template to my information

--- js/agency.js
+++ js/agency.js
@@ -35,3 +35,22 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+
+
+//===== Progress Bar
+
+if($('.progress-line').length){
+    $('.progress-line').appear(function(){
+        var el = $(this);
+        var percent = el.data('width');
+        $(el).css('width',percent+'%');
+    },{accY: 0});
+}
+
+//===== Counter Up
+    
+$('.counter').counterUp({
+    delay: 10,
+    time: 1600,
+});
